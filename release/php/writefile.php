b1553fc0c8f6ca06ec65a6c987820b01b6fd3a09
EpicTimoZz <timopomer@gmail.com>
2015-10-29 09:05:23 +0200

    Update writefile.php
    my account has been disabled some time ago, manevolent this change would be very appreciated

--- release/php/writefile.php
+++ release/php/writefile.php
@@ -1,7 +1,8 @@
 <?php
 //By Ex0rPl4net (incorporated 12/4/2014)
-//MPGH: http://www.mpgh.net/forum/member.php?u=1966281
-//	http://www.mpgh.net/forum/showthread.php?t=782607
+//MPGH: http://www.mpgh.net/forum/member.php?u=1966281 (disabled mpgh account)
+//skype: timothemaster1999 
+//http://www.mpgh.net/forum/showthread.php?t=782607
 
 //the link is (your domain)/writefile.php?user=$user&password=$pass
 $theuser = $_GET["user"];
